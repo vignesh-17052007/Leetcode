f6a6e929751c2c1e73b37efe4decdbff4c580c96
Vignesh <vignesh936069@gmail.com>
2026-03-29 16:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (33.54%) - LeetSync

---
+++ 2955-account-balance-after-rounded-purchase/account-balance-after-rounded-purchase.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+
+        int rounded = ((purchaseAmount + 5) / 10) * 10;
+
+        return 100 - rounded;
+    }
+}
